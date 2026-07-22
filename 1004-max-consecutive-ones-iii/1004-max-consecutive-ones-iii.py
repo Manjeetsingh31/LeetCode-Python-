81bbd68330236a202700c491d5b7449369e69880
Manjeet kumar <168326863+Manjeetsingh31@users.noreply.github.com>
2026-07-22 15:12:29 +0530

    Time: 71 ms (19.37%), Space: 22.3 MB (6.62%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -7,7 +7,7 @@ class Solution:
         while right < len(nums):
             if nums[right] == 0:
                 zero +=1
-            if zero > k:
+            while zero > k:
                 if nums[left] == 0:
                     zero-=1
                 left +=1 
